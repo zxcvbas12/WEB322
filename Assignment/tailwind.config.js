4fc17084048d311c55efb6593b39bd9877dbcf09
Ji Ho Nam <wlgh0123456@gmail.com>
2023-10-24 22:36:46 -0400

    update

---
+++ Assignment/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [`./views/*.html`],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["cupcake"],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
